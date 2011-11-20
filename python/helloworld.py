2974da854f504e11977377557764b0e8656be83e
daemonTutorials <maik.woehl@web.de>
2011-11-20 16:34:58 +0100

    add .gitignore and first item

---
+++ python/helloworld.py
@@ -0,0 +1,14 @@
+import Tkinter
+from Tkconstants import *
+
+# Main
+def main():
+    root = Tkinter.Tk()
+    root.title("Hello World")
+    
+    button = Tkinter.Button(root, text="Hello World!")
+    button.pack()
+    
+    root.mainloop()
+    
+main()
