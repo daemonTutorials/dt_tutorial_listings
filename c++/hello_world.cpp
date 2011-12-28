4da69d59e0cdf3793d4f54475e19397751b9fba4
Maik Wöhl <maik.woehl@web.de>
2011-12-28 12:53:12 +0100

    remove commentlines in cpp program

--- c++/hello_world.cpp
+++ c++/hello_world.cpp
@@ -7,13 +7,13 @@ using namespace std;
 int main()
 {
     // Variablen
-    // int i = 1;
+    int i = 1;
     char Name1[128];
     
     // Execute
-    /* cout << "Hello World!" << endl;
+    cout << "Hello World!" << endl;
     cout << "I'm Maik. I written a executable cpp file." << endl;
-    cout << "Wert der Variable i: " << i << endl; */
+    cout << "Wert der Variable i: " << i << endl;
     cout << "Dein Name: ";
     cin >> Name1;
     cout << "Eingegebener Name: " << Name1 << endl;
