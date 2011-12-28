a5815472b0fbec951595242507ee0aaca30e2cd9
Maik Wöhl <maik.woehl@web.de>
2011-12-28 12:48:07 +0100

    files for c++ tutorial

---
+++ c++/hello_world.cpp
@@ -0,0 +1,23 @@
+// C++ Hello World
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // Variablen
+    // int i = 1;
+    char Name1[128];
+    
+    // Execute
+    /* cout << "Hello World!" << endl;
+    cout << "I'm Maik. I written a executable cpp file." << endl;
+    cout << "Wert der Variable i: " << i << endl; */
+    cout << "Dein Name: ";
+    cin >> Name1;
+    cout << "Eingegebener Name: " << Name1 << endl;
+    
+    return 0;
+}
+
---
+++ python/cpp_vergleich.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def main():
+    name = input(" Dein Name: ")
+    print("Eingegebener Name: " + name)
+    return 0;
+
+main()
+
+
